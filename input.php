7f7a0af262dd30d33342cf5c16473cc7dd6c468e
anje <anjepn@gmail.com>
2016-10-22 07:37:40 -0700

    test

---
+++ input.php
@@ -0,0 +1,10 @@
+<?php
+	$num1=	$_POST['num1'];
+	$num2= $_POST['num2'];
+
+	echo $num1 * $num2;
+
+
+
+
+?>
---
+++ test.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>product</title></head>
+
+
+<body>
+<form method="POST" action="input.php">
+	<input type="text" name="num1"/>
+	<input type="text" name="num2"/>
+	<input type="submit" value="calculate"/>
+	
+</form>
+</body>
+</html>
